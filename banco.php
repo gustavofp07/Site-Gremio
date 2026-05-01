bb2feb163e4e91e1809c56b321fa192b835f6199
Gustavo <gustavopedrosafusca@gmail.com>
2026-04-30 23:27:56 -0300

    primeiro contato

--- banco.php
+++ banco.php
@@ -5,14 +5,13 @@
         $conexao = null;
         try{	        
             //Local
-            //$conexao = new PDO('mysql:host=localhost; dbname='', '', '');
-            $conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conexao->exec("SET NAMES 'utf8mb4'");
+            $conexao = new PDO('mysql:host=localhost; dbname=teste', 'root', '');
+            //$conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            //$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //$conexao->exec("SET NAMES 'utf8mb4'");
 
 		}catch (PDOException $erro){
             echo $erro;
-            
             die();
         }	        
         return $conexao;
